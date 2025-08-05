4c8060f9e7d99be57fe3d1da32ecf8781ed9ae14
radinals <radinalsaragih@gmail.com>
2025-08-05 16:43:35 +0700

    changed volume control commands

--- module/keybindings/commands.lua
+++ module/keybindings/commands.lua
@@ -5,9 +5,9 @@ return {
   increaseBrightness        =  function(n) os.execute("brightnessctl s ".. n .."%+") end,
   decreaseBrightness        =  function(n) os.execute("brightnessctl s ".. n .."%-") end,
 
-  increaseAudioVolume       =  function(n) os.execute("pactl set-sink-volume @DEFAULT_SINK@ +" .. n .. "%") end,
-  decreaseAudioVolume       =  function(n) os.execute("pactl set-sink-volume @DEFAULT_SINK@ -" .. n .. "%") end,
-  toggleAudioMute           =  function()  os.execute("pactl set-sink-mute @DEFAULT_SINK@ toggle") end,
+  increaseAudioVolume       =  function(n) os.execute("volumecontrol up " .. n ) end,
+  decreaseAudioVolume       =  function(n) os.execute("volumecontrol down " .. n) end,
+  toggleAudioMute           =  function()  os.execute("volumecontrol mute") end,
 
   cmusNextTrack             =  function()  os.execute("cmus-remote --next") end,
   cmusPrevTrack             =  function()  os.execute("cmus-remote --prev") end,
