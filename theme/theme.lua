a378cfa88e6c8c3edfa29688b08f518748eb9dbc
radinals <radinalsaragih@gmail.com>
2025-08-05 16:40:40 +0700

    disabled promptbox

--- theme/theme.lua
+++ theme/theme.lua
@@ -133,7 +133,7 @@ theme.at_screen_connect = function(s)
   -- Each screen has its own tag table.
 
   -- Create a promptbox for each screen
-  s.mypromptbox = awful.widget.prompt()
+  -- s.mypromptbox = awful.widget.prompt()
   -- Create an imagebox widget which will contain an icon indicating which layout we're using.
   -- We need one layoutbox per screen.
   s.mylayoutbox = awful.widget.layoutbox(s)
