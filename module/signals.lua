ead01330cb1a9744b53cfd0de8517ac08834fcd3
radinals <radinalsaragih@gmail.com>
2025-08-05 16:42:39 +0700

    added fallback handler for clients without icons

--- module/signals.lua
+++ module/signals.lua
@@ -16,6 +16,21 @@ local manageClient = function (c)
 
 end
 
+local iconPath = "/usr/share/icons/gnome/256x256/"
+
+local fallbackIcons = function(c)
+    local cairo = require("lgi").cairo
+    local default_icon = iconPath.."apps/utilities-terminal.png"
+    if c and c.valid and not c.icon then
+        local s = gears.surface(default_icon)
+        local img = cairo.ImageSurface.create(cairo.Format.ARGB32, s:get_width(), s:get_height())
+        local cr = cairo.Context(img)
+        cr:set_source_surface(s, 0, 0)
+        cr:paint()
+        c.icon = img._native
+    end
+end
+
 local at_titlebar_request = function(c)
   print("INREQ")
   -- buttons for the titlebar
@@ -93,5 +108,11 @@ return {
     signal="request::titlebars",
     callback=function(c) at_titlebar_request(c) end
   },
+
+  {
+    call = client.connect_signal,
+    signal = "manage",
+    callback = function (c) fallbackIcons(c) end
+  }
 
 }
