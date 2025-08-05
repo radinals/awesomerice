204f1c4dd343ee710ec22bb7a0594113ee061d21
radinals <radinalsaragih@gmail.com>
2025-08-05 16:43:07 +0700

    updates to client rules

--- module/rules.lua
+++ module/rules.lua
@@ -45,13 +45,17 @@ return {
       "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
       "Wpa_gui",
       "veromix",
-      "xtightvncviewer"},
+      "xtightvncviewer",
+      "Dialog",
+      "TaskMan"
+    },
 
     -- Note that the name property shown in xprop might be set slightly after creation of the client
     -- and the name shown there might not match defined rules here.
     name = {
       "Event Tester",  -- xev.
       "Eclipse Marketplace ",
+      "Clocks",
     },
     role = {
       "AlarmWindow",  -- Thunderbird's calendar.
@@ -65,17 +69,45 @@ return {
     },
   },
 
-  -- Add titlebars to normal clients and dialogs
-  { rule_any = {type = { "normal", "dialog" }
-  }, properties = { titlebars_enabled = true }
+  {
+    rule_any = {
+      type = {
+        "normal"
+      }
+    },
+    properties = {
+      titlebars_enabled = true
+    }
   },
 
-  { rule_any = { class = {"mpv"}, },
-    properties = { size_hints_honor=false, titlebars_enabled=false }
+  {
+    rule_any = {
+      name = { "Lutris" },
+      class = {"Zathura"}
+    },
+    properties = {
+      titlebars_enabled = false
+    }
   },
 
-  { rule_any = { class = {"st-256color"}, name = {"st"} },
-    properties = { size_hints_honor=false, titlebars_enabled=false }
+  {
+    rule_any = {
+      class = {"mpv"},
+    },
+    properties = {
+      size_hints_honor=false,
+      titlebars_enabled=false
+    }
+  },
+
+  { 
+    rule_any = {
+      class = {"st-256color"},
+      name = {"st"} },
+    properties = {
+      size_hints_honor=false,
+      titlebars_enabled=false
+    }
   },
 
   -- Set Firefox to always map on the tag named "2" on screen 1.
